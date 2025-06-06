8b295cee91db3dc5b1a55f715b51dbaf0152c1fa
pantasuman6 <pantasuman6@gmail.com>
2025-06-06 16:59:03 -0400

    fixed bug

--- BinarySearch.js
+++ BinarySearch.js
@@ -2,6 +2,11 @@ const sArray =[2, 4, 12, 20, 45, 55, 67, 78, 84 ]
 
 function binarySearch(arr, low, high, x)  {
 
+    if(low>high)  {
+
+        return 'Element not Found!'
+    }
+
     let mid = Math.floor((low+high)/2);
 
     if (arr[mid]==x) {
@@ -17,10 +22,10 @@ return binarySearch(arr, low, mid-1, x);
 
     else{
 
-return binarySearch(arr, low, mid+1, x);
+return binarySearch(arr, mid+1, high, x);
     }
 }
 
-const result = binarySearch(sArray, 0, sArray.length-1, 4)
+const result = binarySearch(sArray, 0, sArray.length-1, 84)
 
 console.log(result);
